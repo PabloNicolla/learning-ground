a749fba4953d01ef9e68e4d1ffe994a82572f001
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-19 15:11:00 -0400

    LC 121

---
+++ LC/121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_p, min_p, max_profit = prices[0], prices[0], 0
+
+        for p in prices:
+            if p < min_p:
+                min_p = p
+                max_p = p
+            else:
+                max_p = max(max_p, p)
+            max_profit = max(max_profit, max_p-min_p)
+
+        return max_profit
