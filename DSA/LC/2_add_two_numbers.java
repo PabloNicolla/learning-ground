1301e403ba69035be1058e96e6eb580c4656f17f
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-27 16:26:01 -0400

    LC 2

---
+++ DSA/LC/2_add_two_numbers.java
@@ -0,0 +1,35 @@
+public class ListNode {
+  int val;
+  ListNode next;
+  ListNode() {}
+  ListNode(int val) { this.val = val; }
+  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode dummy = new ListNode();
+    ListNode currNode = dummy;
+    int carry = 0;
+
+    while (carry != 0 || l1 != null || l2 != null) {
+      int l1Value = 0;
+      int l2Value = 0;
+      if (l1 != null) {
+        l1Value = l1.val;
+        l1 = l1.next;
+      }
+      if (l2 != null) {
+        l2Value = l2.val;
+        l2 = l2.next;
+      }
+      int newValue = carry + l1Value + l2Value;
+      carry = newValue / 10;
+      newValue %= 10;
+      currNode.next = new ListNode(newValue);
+      currNode = currNode.next;
+    }
+
+    return  dummy.next;
+  }
+}
