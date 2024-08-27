e90ad3ae92348f20d204a21acb5d0efbbaaa7045
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-27 16:03:01 -0400

    LC 138 PY

---
+++ DSA/LC/138_copy_list_with_random_pointer copy.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head: Optional[Node]) -> Optional[Node]:
+        # Original Address -> Copy Address
+        old_to_copy = {None: None}
+
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            old_to_copy[cur] = copy
+            cur = cur.next
+        cur = head
+        while cur:
+            copy = old_to_copy[cur]
+            copy.next = old_to_copy[cur.next]
+            copy.random = old_to_copy[cur.random]
+            cur = cur.next
+        return old_to_copy[head]
