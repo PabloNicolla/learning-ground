61ac5eeec8243359b837df755aac872cd0f95e82
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-28 02:56:59 -0400

    LC 981

---
+++ DSA/LC/981_time_based_key-value_store.cpp
@@ -0,0 +1,64 @@
+#include <string>
+#include <unordered_map>
+
+struct Data
+{
+  int timestamp{};
+  std::string value{};
+};
+
+class TimeMap
+{
+  std::unordered_map<std::string, std::vector<Data>> map{};
+
+public:
+  TimeMap()
+  {
+  }
+
+  void set(std::string key, std::string value, int timestamp)
+  {
+    map[key].push_back({timestamp, value});
+  }
+
+  std::string get(std::string key, int timestamp)
+  {
+    const auto iterator = map.find(key);
+    if (iterator == map.end())
+    {
+      return "";
+    }
+
+    const auto &vec = iterator->second;
+
+    if (vec[0].timestamp > timestamp)
+    {
+      return "";
+    }
+
+    int start = 0;
+    int end = vec.size() - 1;
+    int mid{};
+    int prevSmallerTimeIndex{};
+
+    while (start <= end)
+    {
+      mid = (end - start) / 2 + start;
+      if (vec[mid].timestamp == timestamp)
+      {
+        return vec[mid].value;
+      }
+      else if (timestamp > vec[mid].timestamp)
+      {
+        start = mid + 1;
+      }
+      else
+      {
+        prevSmallerTimeIndex = mid;
+        end = mid - 1;
+      }
+    }
+
+    return vec[prevSmallerTimeIndex].value;
+  }
+};
