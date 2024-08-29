57935e16707e19a6e4c6dc47d2017028db412c04
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-28 22:00:52 -0400

    LC 1448

---
+++ DSA/LC/1448_count_good_nodes_in_binary_tree.cpp
@@ -0,0 +1,41 @@
+#include <functional>
+
+struct TreeNode;
+
+class Solution
+{
+public:
+  int goodNodes(TreeNode *root)
+  {
+    int count{};
+
+    std::function<void(TreeNode * root, int maxSoFar)> rec = [&](TreeNode *root, int maxSoFar)
+    {
+      if (root == nullptr)
+        return;
+
+      if (maxSoFar < root->val)
+      {
+        ++count;
+        maxSoFar = root->val;
+      }
+
+      rec(root->left, maxSoFar);
+      rec(root->right, maxSoFar);
+    };
+
+    rec(root, std::numeric_limits<int>().min());
+
+    return count;
+  }
+};
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
