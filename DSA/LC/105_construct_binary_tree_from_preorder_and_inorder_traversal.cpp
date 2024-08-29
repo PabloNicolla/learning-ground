61df5725e721c323b9150500f09d1dea3751c286
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-29 00:48:34 -0400

    LC 105 PY

--- DSA/LC/105_construct_binary_tree_from_preorder_and_inorder_traversal.cpp
+++ DSA/LC/105_construct_binary_tree_from_preorder_and_inorder_traversal.cpp
@@ -7,46 +7,45 @@ struct TreeNode;
 class Solution
 {
 public:
-    TreeNode *buildTree(std::vector<int> &preorder, std::vector<int> &inorder)
+  TreeNode *buildTree(std::vector<int> &preorder, std::vector<int> &inorder)
+  {
+    std::unordered_set<int> alreadyAdded{};
+    TreeNode *root{};
+    int pIndex{};
+    int iIndex{};
+
+    std::function<TreeNode *(TreeNode * &root)> rec = [&](TreeNode *&root) -> TreeNode *
     {
-        std::unordered_set<int> alreadyAdded{};
-        TreeNode *root{};
-        int pIndex{};
-        int iIndex{};
-        bool isSwapped{false};
+      if (pIndex == preorder.size())
+        return nullptr;
 
-        std::function<TreeNode *(TreeNode * &root)> rec = [&](TreeNode *&root) -> TreeNode *
-        {
-            if (pIndex == preorder.size())
-                return nullptr;
+      if (alreadyAdded.find(inorder[iIndex]) == alreadyAdded.end())
+      {
+        root = new TreeNode(preorder[pIndex]);
+        alreadyAdded.insert(root->val);
+        pIndex++;
+        root->left = rec(root->left);
+      }
 
-            if (alreadyAdded.find(inorder[iIndex]) == alreadyAdded.end())
-            {
-                root = new TreeNode(preorder[pIndex]);
-                alreadyAdded.insert(root->val);
-                pIndex++;
-                root->left = rec(root->left);
-            }
+      if (root != nullptr && inorder[iIndex] == root->val)
+      {
+        iIndex++;
+        root->right = rec(root->right);
+      }
 
-            if (root != nullptr && inorder[iIndex] == root->val)
-            {
-                iIndex++;
-                root->right = rec(root->right);
-            }
+      return root;
+    };
 
-            return root;
-        };
-
-        return rec(root);
-    }
+    return rec(root);
+  }
 };
 
 struct TreeNode
 {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
