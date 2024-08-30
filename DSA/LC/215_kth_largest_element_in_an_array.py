3919ef9e357cff52629d2009a60213e6d5572c9d
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-08-29 20:14:13 -0400

    LC 245 PY

---
+++ DSA/LC/215_kth_largest_element_in_an_array.py
@@ -0,0 +1,34 @@
+# Quick Select
+# Time complexity: O(n) in average, O(n^2) in worst case
+from typing import List
+
+
+class Solution:
+
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+        left, right = 0, len(nums) - 1
+
+        while left < right:
+            pivot = self.partition(nums, left, right)
+
+            if pivot < k:
+                left = pivot + 1
+            elif pivot > k:
+                right = pivot - 1
+            else:
+                break
+
+        return nums[k]
+
+    def partition(self, nums: List[int], left: int, right: int) -> int:
+        pivot, fill = nums[right], left
+
+        for i in range(left, right):
+            if nums[i] <= pivot:
+                nums[fill], nums[i] = nums[i], nums[fill]
+                fill += 1
+
+        nums[fill], nums[right] = nums[right], nums[fill]
+
+        return fill
