01dbb3b8e42ecba8ed6b9aff3569e92b10ced7d3
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-09-03 07:31:34 -0400

    LC 7

---
+++ DSA/LC/7_reverse_integer.cpp
@@ -0,0 +1,48 @@
+class Solution1
+{
+public:
+    int reverse(int x)
+    {
+        const int MIN = -2147483648; // -2^31
+        const int MAX = 2147483647;  // 2^31 - 1
+
+        int res = 0;
+        while (x != 0)
+        {
+            int digit = x % 10;
+            x /= 10;
+
+            if (res > MAX / 10 || (res == MAX / 10 && digit > MAX % 10))
+                return 0;
+            if (res < MIN / 10 || (res == MIN / 10 && digit < MIN % 10))
+                return 0;
+            res = (res * 10) + digit;
+        }
+
+        return res;
+    }
+};
+
+class Solution2
+{
+public:
+    int reverse(int x)
+    {
+        long long ans{};
+        while (x != 0)
+        {
+            ans *= 10;
+            ans += x % 10;
+            x /= 10;
+        }
+        if (ans > 0 && ans > std::numeric_limits<int>().max())
+        {
+            return 0;
+        }
+        else if (ans < 0 && ans < std::numeric_limits<int>().min())
+        {
+            return 0;
+        }
+        return static_cast<int>(ans);
+    }
+};
