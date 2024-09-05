0afed3e2bcb3712b196edbc369d82d7869218d13
PabloNicolla <nicola_pablo@yahoo.com.br>
2024-09-04 20:42:13 -0400

    LC 261 PY V2

--- DSA/LC/261_graph_valid_tree.py
+++ DSA/LC/261_graph_valid_tree.py
@@ -25,3 +25,29 @@ class Solution:
                     used_edges.add((node_to, node))
 
         return True if len(visited_nodes) == len(nodes) else False
+
+
+class Solution2:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if not n:
+            return True
+        adj = {i: [] for i in range(n)}
+        for n1, n2 in edges:
+            adj[n1].append(n2)
+            adj[n2].append(n1)
+
+        visit = set()
+
+        def dfs(i: int, prev: int) -> bool:
+            if i in visit:
+                return False
+
+            visit.add(i)
+            for j in adj[i]:
+                if j == prev:
+                    continue
+                if not dfs(j, i):
+                    return False
+            return True
+
+        return dfs(0, -1) and n == len(visit)
