78cfb59ec1c5503301c670f152bb6878ea72fdcf
Pablo Nicola <pablofnic@gmail.com>
2024-09-07 19:07:36 -0400

    LC 49 V2

--- DSA/LC/49_group_anagrams.cpp
+++ DSA/LC/49_group_anagrams.cpp
@@ -5,7 +5,41 @@
 #include <string>
 #include <unordered_map>
 
-class Solution
+class Solution1
+{
+public:
+  std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string> &strs)
+  {
+    std::unordered_map<std::string, std::vector<std::string>> ans;
+
+    for (auto &s : strs)
+    {
+      std::vector<int> count(26, 0);
+      for (char c : s)
+      {
+        count[c - 'a']++;
+      }
+
+      std::string key;
+      for (int i : count)
+      {
+        key += "#" + std::to_string(i);
+      }
+
+      ans[key].push_back(s);
+    }
+
+    std::vector<std::vector<std::string>> result;
+    for (auto &pair : ans)
+    {
+      result.push_back(pair.second);
+    }
+
+    return result;
+  }
+};
+
+class Solution2
 {
 public:
   std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string> &strs)
