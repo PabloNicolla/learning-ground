69b3c035180989923c2e3fcaf64a04a0c97b6eef
Pablo Nicola <pablofnic@gmail.com>
2024-09-11 04:18:11 -0400

    424 V3

--- DSA/LC/424_longest_repeating_character_replacement.c
+++ DSA/LC/424_longest_repeating_character_replacement.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-int characterReplacement(char *s, int k)
+int characterReplacement2(char *s, int k)
+{
+  int count[26] = {0};
+  int maxCount = 0;
+  int maxLen = 0;
+  int left = 0;
+
+  for (int right = 0; s[right] != '\0'; ++right)
+  {
+    if (maxCount > ++count[s[right] - 'A'])
+    {
+      maxCount = count[s[right] - 'A'];
+    }
+
+    int currLen = (right - left + 1);
+
+    while (currLen - maxCount > k)
+    {
+      count[s[left] - 'A']--;
+      left++;
+    }
+
+    int newLen = (right - left + 1);
+
+    if (maxLen > newLen)
+    {
+      maxLen = newLen;
+    }
+  }
+
+  return maxLen;
+}
+
+int characterReplacement1(char *s, int k)
 {
   int count[26] = {0};
   int maxCount = 0;
