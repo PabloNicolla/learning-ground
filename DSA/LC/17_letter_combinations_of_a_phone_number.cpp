62a30279b0ef8f1fa774b4d432cb112885205310
Pablo Nicola <pablofnic@gmail.com>
2024-09-27 15:16:53 -0400

    LC iterative C++

--- DSA/LC/17_letter_combinations_of_a_phone_number.cpp
+++ DSA/LC/17_letter_combinations_of_a_phone_number.cpp
@@ -43,3 +43,45 @@ public:
         return ans;
     }
 };
+
+class Solution2
+{
+public:
+    std::vector<std::string> letterCombinations(std::string digits)
+    {
+        if (digits.empty())
+        {
+            return {};
+        }
+
+        std::vector<std::string> ans{};
+        const std::unordered_map<char, std::vector<char>> phone_map = {
+            {'2', {'a', 'b', 'c'}},
+            {'3', {'d', 'e', 'f'}},
+            {'4', {'g', 'h', 'i'}},
+            {'5', {'j', 'k', 'l'}},
+            {'6', {'m', 'n', 'o'}},
+            {'7', {'p', 'q', 'r', 's'}},
+            {'8', {'t', 'u', 'v'}},
+            {'9', {'w', 'x', 'y', 'z'}}};
+
+        ans.push_back("");
+
+        for (char digit : digits)
+        {
+            std::vector<std::string> temp_ans{};
+            for (const std::string &str : ans)
+            {
+                for (const char letter : phone_map.at(digit))
+                {
+                    std::string copy = str;
+                    copy.push_back(letter);
+                    temp_ans.emplace_back(std::move(copy));
+                }
+            }
+            ans = std::move(temp_ans);
+        }
+
+        return ans;
+    }
+};
