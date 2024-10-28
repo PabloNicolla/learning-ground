6bb80c008341044ed3c35d5f1086d6f0843d0e0c
Pablo Nicola <pablofnic@gmail.com>
2024-10-28 19:43:48 -0400

    LC 55

---
+++ DSA/LC/55_jump_game.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+
+class Solution
+{
+public:
+    bool canJump(std::vector<int> &nums)
+    {
+        int goal = nums.size() - 1;
+
+        for (int i = nums.size() - 2; i >= 0; i--)
+        {
+            if (i + nums[i] >= goal)
+            {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+};
